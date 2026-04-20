f445389f55f0b72b55564365459c8463c51b53cd
Unwuchola, Eneojo <eneojounwuchola@my.unt.edu>
2026-04-20 18:23:55 -0500

    modularize prompts

--- main.go
+++ main.go
@@ -25,19 +25,18 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Could not get device info: ", err)
 		os.Exit(1)
 	}
+	displayPrompt()
 
-	fmt.Println("\nWelcome to GoWhoAteMyCPU!")
-	fmt.Println("")
-	fmt.Println("More info on features:")
-	fmt.Println("https://github.com/buildingBuild/GoWhoAteMyCPU")
-	fmt.Println("")
-	displayDeviceInfo(deviceInfo)
-
-	fmt.Println(Green + "\nOPTIONS: " + Reset)
-	fmt.Println("1. CPU Monitoring Mode:")
-	fmt.Println("2. Memory Monitoring Mode:")
-	fmt.Println("3. Network Monitoring Mode:")
-	fmt.Println("5. Hybrid Monitoring Mode:")
-	fmt.Print("Select a Mode: ")
-	fmt.Scan(&mode)
+	switch mode {
+	case 1:
+		fmt.Println("CPU Monitoring Mode: ")
+	case 2:
+		fmt.Println("Memory Monitoring Mode: ")
+	case 3:
+		fmt.Println("Network Monitoring Mode: ")
+	case 5:
+		fmt.Println("Hybrid Monitoring Mode: ")
+	default:
+		fmt.Println("")
+	}
 }
---
+++ prompt.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func displayPrompt() {
+
+	fmt.Println("\nWelcome to GoWhoAteMyCPU!")
+	fmt.Println("")
+	fmt.Println("More info on features:")
+	fmt.Println("https://github.com/buildingBuild/GoWhoAteMyCPU")
+	fmt.Println("")
+	displayDeviceInfo(deviceInfo)
+
+	fmt.Println(Green + "\nOPTIONS: " + Reset)
+	fmt.Println("1. CPU Monitoring Mode:")
+	fmt.Println("2. Memory Monitoring Mode:")
+	fmt.Println("3. Network Monitoring Mode:")
+	fmt.Println("5. Hybrid Monitoring Mode:")
+	fmt.Println("6. Rewind Time:")
+	fmt.Print("Select a Mode: ")
+
+}
